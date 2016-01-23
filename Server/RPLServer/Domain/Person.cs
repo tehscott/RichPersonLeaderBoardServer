947caa63f91d3c8313a86f2511a4f470dc03e0cf
theTrueMikeBrown <theTrueMikeBrown@gmail.com>
2016-01-22 21:25:43 -0700

    Added a controller with an index method that makes me the richest

---
+++ Server/RPLServer/Domain/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public decimal Wealth { get; set; }
+    }
+}
---
+++ Server/RPLServer/Server/Controllers/LeaderboardController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain;
+using System.Web.Mvc;
+
+namespace Server.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        public List<Person> Persons {
+            get
+            {
+                return new List<Person>
+                {
+                    new Person {Name = "theTrueMikeBrown", Wealth = 100.00M},
+                    new Person {Name = "tehScott", Wealth = 50.00M},
+                    new Person {Name = "mattiam", Wealth = 0.00M}
+                };
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return Json(Persons, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
