c1a00f1531faf63e5df7a4a800a69950790f7f6f
Scott Stromberg <srstromberg@gmail.com>
2016-01-23 16:52:09 -0700

    set up a basic UI, not done.

--- Android/app/src/main/java/com/mattandmikeandscott/richpersonleaderboard/MainActivity.java
+++ Android/app/src/main/java/com/mattandmikeandscott/richpersonleaderboard/MainActivity.java
@@ -1,15 +1,139 @@
 package com.mattandmikeandscott.richpersonleaderboard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Test change
+        // Set up the action bar.
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        hideActionBarTitle(actionBar);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            //.setIcon(mSectionsPagerAdapter.getPageIcon(i))
+                            .setTabListener(this));
+        }
     }
+
+    private void hideActionBarTitle(ActionBar actionBar) {
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a MainFragment (defined as a static inner class below).
+            return MainFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "ALL TIME";
+                case 1:
+                    return "TODAY";
+                case 2:
+                    return "THIS WEEK";
+                case 3:
+                    return "THIS MONTH";
+                case 4:
+                    return "THIS YEAR";
+                default:
+                    return "";
+            }
+        }
+
+        //public int getPageIcon(int position) {
+        //    switch (position) {
+        //        case 0:
+        //            return R.drawable.table_icon;
+        //        case 1:
+        //            return R.drawable.boss_icon;
+        //        case 2:
+        //            return R.drawable.home_icon;
+        //        case 3:
+        //            return R.drawable.cards_icon;
+        //        default:
+        //            return -1;
+        //    }
+        //}
+    }
+
+
+
+
+
 }
