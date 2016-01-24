354ab5318233d9828995497949261f64fb15ced1
theTrueMikeBrown <theTrueMikeBrown@gmail.com>
2016-01-23 22:19:49 -0700

    Adding a db to the server and making a Dao that builds the first table if it doesn't exist. I added some dummy data to the table right now, but we will probably want to change the table, so I didn't go crazy.
    http://richpersonleaderboardserver.azurewebsites.net/leaderboard/

--- Server/RPLServer/Server/Controllers/LeaderboardController.cs
+++ Server/RPLServer/Server/Controllers/LeaderboardController.cs
@@ -4,26 +4,22 @@ using System.Linq;
 using System.Web;
 using Domain;
 using System.Web.Mvc;
+using Data;
 
 namespace Server.Controllers
 {
     public class LeaderboardController : Controller
     {
-        public List<Person> Persons {
-            get
-            {
-                return new List<Person>
-                {
-                    new Person {Name = "theTrueMikeBrown", Wealth = 100.00M},
-                    new Person {Name = "tehScott", Wealth = 50.00M},
-                    new Person {Name = "mattiam", Wealth = 0.00M}
-                };
-            }
+        public PersonDao Dao { get; set; }
+
+        public LeaderboardController()
+        {
+            Dao = new PersonDao();
         }
 
         public ActionResult Index()
         {
-            return Json(Persons, JsonRequestBehavior.AllowGet);
+            return Json(Dao.GetPersons(0, 100), JsonRequestBehavior.AllowGet);
         }
     }
 }
