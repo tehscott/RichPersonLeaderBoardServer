2403761ec25dcd35fccd51f8d3655505ff78bf63
tehscott <srstromberg@gmail.com>
2016-01-25 13:32:01 -0700

    Working on adding pulling the list from the azure website. Not working yet.

--- Android/app/src/main/java/com/mattandmikeandscott/richpersonleaderboard/MainFragment.java
+++ Android/app/src/main/java/com/mattandmikeandscott/richpersonleaderboard/MainFragment.java
@@ -1,13 +1,13 @@
 package com.mattandmikeandscott.richpersonleaderboard;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import com.mattandmikeandscott.richpersonleaderboard.adapters.PersonListAdapter;
 import com.mattandmikeandscott.richpersonleaderboard.domain.PeopleQueryType;
 import com.mattandmikeandscott.richpersonleaderboard.domain.Person;
 import com.mattandmikeandscott.richpersonleaderboard.network.Repository;
@@ -15,17 +15,8 @@ import com.mattandmikeandscott.richpersonleaderboard.network.Repository;
 import java.util.ArrayList;
 
 public class MainFragment extends Fragment {
-    /**
-     * The fragment argument representing the section number for this
-     * fragment.
-     */
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-
-    /**
-     * Returns a new instance of this fragment for the given section
-     * number.
-     */
     public static MainFragment newInstance(int sectionNumber) {
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
@@ -37,32 +28,49 @@ public class MainFragment extends Fragment {
     public MainFragment() {}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
         View rootView = null;
         int sectionNumber = getArguments().getInt(ARG_SECTION_NUMBER);
 
         switch (sectionNumber) {
             case 0:
-                rootView = inflater.inflate(R.layout.fragment_all_time, container, false);
+                rootView = inflater.inflate(R.layout.fragment_list, container, false);
 
-                ListView list = (ListView) rootView.findViewById(R.id.list);
+                Message message = new Message();
+                message.obj = true;
+                message.what = MainActivity.HandlerResult.TOGGLE_LOADING.ordinal();
+                ((MainActivity)getActivity()).handler.sendMessage(message);
 
-                Repository repository = new Repository();
-                ArrayList<Person> people = repository.getPeople(PeopleQueryType.AllTime);
-                list.setAdapter(new PersonListAdapter(getContext(), people));
+                final View fragment = rootView;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Repository repository = new Repository();
+                        ArrayList<Person> people = repository.getPeople(PeopleQueryType.AllTime);
+
+                        ListView list = (ListView) fragment.findViewById(R.id.list);
+                        Object[] parameters = new Object[] { getActivity(), list, people };
+
+                        Message message = new Message();
+                        message.what = MainActivity.HandlerResult.PERSON_INFO_AQUIRED.ordinal();
+                        message.obj = parameters;
+
+                        ((MainActivity)getActivity()).handler.sendMessage(message);
+                    }
+                }).start();
 
                 break;
             case 1:
-                rootView = inflater.inflate(R.layout.fragment_today, container, false);
+                rootView = inflater.inflate(R.layout.fragment_list, container, false);
                 break;
             case 2:
-                rootView = inflater.inflate(R.layout.fragment_this_week, container, false);
+                rootView = inflater.inflate(R.layout.fragment_list, container, false);
                 break;
             case 3:
-                rootView = inflater.inflate(R.layout.fragment_this_month, container, false);
+                rootView = inflater.inflate(R.layout.fragment_list, container, false);
                 break;
             case 4:
-                rootView = inflater.inflate(R.layout.fragment_this_year, container, false);
+                rootView = inflater.inflate(R.layout.fragment_list, container, false);
                 break;
         }
 
