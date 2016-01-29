9ad88aadbcf808b080fced4539969589c962cad3
tehscott <srstromberg@gmail.com>
2016-01-28 17:00:18 -0700

    added missing resources

--- Android/app/src/main/java/com/mattandmikeandscott/richpersonleaderboard/Adapters/PersonListAdapter.java
+++ Android/app/src/main/java/com/mattandmikeandscott/richpersonleaderboard/Adapters/PersonListAdapter.java
@@ -1,19 +1,22 @@
 package com.mattandmikeandscott.richpersonleaderboard.adapters;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.mattandmikeandscott.richpersonleaderboard.MainActivity;
 import com.mattandmikeandscott.richpersonleaderboard.R;
 import com.mattandmikeandscott.richpersonleaderboard.domain.Person;
 
 import java.text.NumberFormat;
 import java.util.List;
 
-public class PersonListAdapter extends BaseAdapter implements View.OnClickListener {
+public class PersonListAdapter extends BaseAdapter {
     private Context context;
     private NumberFormat formatter = NumberFormat.getCurrencyInstance();
 
@@ -38,7 +41,7 @@ public class PersonListAdapter extends BaseAdapter implements View.OnClickListen
     }
 
     public View getView(int position, View convertView, ViewGroup viewGroup) {
-        Person currentPerson = people.get(position);
+        final Person currentPerson = people.get(position);
 
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = inflater.inflate(R.layout.list_item, null);
@@ -61,11 +64,26 @@ public class PersonListAdapter extends BaseAdapter implements View.OnClickListen
 
         // Person avatar
 
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPersonProfileDialog(currentPerson.getId());
+            }
+        });
+
         return convertView;
     }
 
-    @Override
-    public void onClick(View v) {
+    private void showPersonProfileDialog(final int personId) {
+        Dialog dialog = new Dialog(context, android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
+        dialog.setCancelable(true);
 
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.person_profile_dialog, null);
+
+        ((TextView) layout.findViewById(R.id.person_profile_header_text)).setText(String.valueOf(personId));
+
+        dialog.setContentView(layout);
+        dialog.show();
     }
 }
