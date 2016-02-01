a449c29c78461a0c2566edecf71870c421f2d47d
Mike Brown <MBrown@1800contacts.com>
2016-02-01 09:20:41 -0700

    Finishing up the manual wealth resetter

--- Server/RPLServer/Processor/WealthResetter.cs
+++ Server/RPLServer/Processor/WealthResetter.cs
@@ -19,26 +19,27 @@ namespace Processor
 
         public void ResetWealth()
         {
-            var date = _dao.GetLastResetDate();
-            if (date.Date < DateTime.Today)
+            var date = _dao.GetLastResetDate().ToLocalTime();
+            var today = DateTime.Today;
+            if (date.Date < today)
             {
-                if (date.Year != DateTime.Today.Year)
+                if (date.Year != today.Year)
                 {
                     _dao.ResetWealth(RankType.Year);
                 }
-                else if (date.Month != DateTime.Today.Month)
+                else if (date.Month != today.Month)
                 {
                     _dao.ResetWealth(RankType.Month);
                 }
                 
-                if (date.DayOfWeek > DateTime.Today.DayOfWeek)
+                if (date.DayOfWeek > today.DayOfWeek)
                 {
                     _dao.ResetWealth(RankType.Week);
                 }
 
-                if (date.Year == DateTime.Today.Year &&
-                    date.Month == DateTime.Today.Month &&
-                    date.DayOfWeek <= DateTime.Today.DayOfWeek)
+                if (date.Year == today.Year &&
+                    date.Month == today.Month &&
+                    date.DayOfWeek <= today.DayOfWeek)
                 {
                     _dao.ResetWealth();
                 }
