baae18f8c7e8efe0ba70394847734039977dd6d4
Mike Brown <MBrown@1800contacts.com>
2016-02-01 11:29:54 -0700

    added google single sign on. needs work.

--- Android/app/src/main/java/com/mattandmikeandscott/richpersonleaderboard/MainActivity.java
+++ Android/app/src/main/java/com/mattandmikeandscott/richpersonleaderboard/MainActivity.java
@@ -1,12 +1,6 @@
 package com.mattandmikeandscott.richpersonleaderboard;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -18,31 +12,40 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.mattandmikeandscott.richpersonleaderboard.adapters.PersonListAdapter;
 import com.mattandmikeandscott.richpersonleaderboard.domain.GetPeopleResponse;
 import com.mattandmikeandscott.richpersonleaderboard.domain.PeopleQueryType;
-import com.mattandmikeandscott.richpersonleaderboard.domain.Person;
 import com.mattandmikeandscott.richpersonleaderboard.domain.RankType;
 
-import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Map;
 
-import static com.mattandmikeandscott.richpersonleaderboard.MainActivity.HandlerResult.*;
+import static com.mattandmikeandscott.richpersonleaderboard.MainActivity.HandlerResult.PEOPLE_INFO_AQUIRED;
 
-public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener, GoogleApiClient.OnConnectionFailedListener {
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private int myId = 1; // TODO: Setup logging in and such
 
+    private static int RC_SIGN_IN = 1000;
+
     private ViewPager mViewPager;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 
     public enum HandlerResult {
         PEOPLE_INFO_AQUIRED, PERSON_INFO_AQUIRED
@@ -91,7 +94,79 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         }
 
         setupButtons();
+
+
+
+
+
+
+
+
+        // Configure sign-in to request the user's ID, email address, and basic profile. ID and
+        // basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to GoogleSignIn.API and the options above.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
     }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(getString(R.string.app_short_name), "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            //updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            //updateUI(false);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private void setupButtons() {
         findViewById(R.id.find_me_button).setOnClickListener(new View.OnClickListener() {
