905a8f3e2162283f4861e4b8e7be060813ee8fdf
Mike Brown <MBrown@1800contacts.com>
2016-03-17 09:50:17 -0600

    Adding the public key thing

--- RPLServer/BusinessTests/Tests.cs
+++ RPLServer/BusinessTests/Tests.cs
@@ -10,7 +10,7 @@ namespace Business.Tests
         [Test]
         public void GetXMLFromKey()
         {
-            var MY_BASE64_PUBLIC_KEY = "INSERT KEY HERE";
+            var MY_BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAl33UVxeoM2D0rjCUQGip7iCHwJrFT8XUiv+y+17eLVdWzC2zUgEJs+f8dGn2KEC7/y5yoJ45cBxg2uMrh6w0e1soKMDP5WuaUkjUFJdYmWRn6HkIIEoS77XUyRYzTDT4QidYj15gYYP0xz9rrE69CSifIcEfWNZRSAq1cK0A5fMHchIenMO2plwLHL0V/BJ8Kx5321uAvhzhjiatS2E3lJ+MZmZV8jJtoMDwpcxVEJyEbvBs1dGkWy776VUVirgeNEWM3jhp9gIPJMSCrF+MUrkCh/HCKKrhSI1dVT2FDd0eEQboFgrdtToqrhwY2qmLNyF3uJbZg+NG4mnrfCdhIwIDAQAB";
             RSACryptoServiceProvider provider = PemKeyLoader.CryptoServiceProviderFromPublicKeyInfo(MY_BASE64_PUBLIC_KEY);
             var xmlString = provider.ToXmlString(false);
             Console.WriteLine(xmlString);
